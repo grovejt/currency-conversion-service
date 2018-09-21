3de7fc5cef02a51d768e5934ca498f75c68fc9f7
grovejt <grovejt@gmail.com>
2018-09-21 13:03:17 -0500

    Hook in Zuul gateway for call from currency-conversion-service to currency-exchange-service.

--- src/main/java/com/grove/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/grove/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -5,12 +5,19 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+// Hard coded location:
 //@FeignClient(name="currency-exchange-service", url="localhost:8000")
-@FeignClient(name="currency-exchange-service")
+
+// Instead of connecting to naming server connect to zuul:
+//@FeignClient(name="currency-exchange-service")
+@FeignClient(name="netflix-zuul-api-gateway-server")
+
 @RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 	
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	//@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	// Add in zule service name to uri since we want to go through zuul instead of the naming server:
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
 	public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 
 }
